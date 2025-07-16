2c4141e46a61d2f9c8ebe2f1ed6358e7459fe4a7
yathish <yathish1047@gmail.com>
2025-07-16 15:14:21 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,16 +1,16 @@
 output "cluster_id" {
-  value = aws_eks_cluster.devopsshack.id
+  value = aws_eks_cluster.yathish.id
 }
 
 output "node_group_id" {
-  value = aws_eks_node_group.devopsshack.id
+  value = aws_eks_node_group.yathish.id
 }
 
 output "vpc_id" {
-  value = aws_vpc.devopsshack_vpc.id
+  value = aws_vpc.yathish_vpc.id
 }
 
 output "subnet_ids" {
-  value = aws_subnet.devopsshack_subnet[*].id
+  value = aws_subnet.yathish_subnet[*].id
 }
 
